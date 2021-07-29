ca5c1a38272d169f1c656712faf8586990306699
OS <oceanshape700@gmail.com>
2021-07-29 18:43:10 +0900

    test: Add line-drawing function to View::mousePressEvent()

--- testSheet/view.cpp
+++ testSheet/view.cpp
@@ -6,4 +6,8 @@ View::View(QWidget *parent) : QGraphicsView(parent)
 }
 
 void View::mousePressEvent(QMouseEvent *event) {
+    QPointF scenePosition = mapToScene(event->pos());
+    QPen* pen = new QPen(Qt::green);
+    pen->setWidth(3);
+    scene()->addLine(scenePosition.x(), scenePosition.y(), scenePosition.x() + 100, scenePosition.y() + 100, *pen);
 }
