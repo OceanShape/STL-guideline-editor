c62d3309223857719abc23a0abe6e23ccb5b5d9e
OS <oceanshape700@gmail.com>
2021-08-04 20:55:43 +0900

    feat: Add removing point function

--- view.cpp
+++ view.cpp
@@ -1,11 +1,21 @@
 #include "view.h"
 
-View::View(QWidget *parent) : QGraphicsView(parent) {}
+std::vector<QGraphicsEllipseItem*> point;
 
-void View::mousePressEvent(QMouseEvent *event) {
+View::View(QWidget* parent) : QGraphicsView(parent) {}
+
+void View::mousePressEvent(QMouseEvent* event) {
   QPointF pos = mapToScene(event->pos());
-  QPen *pen = new QPen(Qt::green);
+  QPen* pen = new QPen(Qt::green);
   QBrush* brush = new QBrush(Qt::white);
   pen->setWidth(10);
-  scene()->addEllipse(pos.x() - 10, pos.y() - 10, 50, 50 , *pen, *brush);
+  if (event->button() == Qt::LeftButton) {
+    point.push_back(
+        scene()->addEllipse(pos.x() - 10, pos.y() - 10, 50, 50, *pen, *brush));
+  } else {
+    if (!point.empty()) {
+      scene()->removeItem((QGraphicsItem*)point.back());
+      point.pop_back();
+    }
+  }
 }
