531610b2a6bc2e79b5fff9f3529812414bb7bd06
OS <oceanshape700@gmail.com>
2021-08-05 17:52:28 +0900

    fix: Fix window closing bug when click graphics scene before image loading

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,11 +9,19 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
   connect(ui.actionNew, SIGNAL(triggered()), this, SLOT(New()));
   connect(ui.actionOpen, SIGNAL(triggered()), this, SLOT(Open()));
   connect(ui.actionClose, SIGNAL(triggered()), this, SLOT(Close()));
+
+  ui.viewAP->setScene(new QGraphicsScene);
+  ui.viewLAT->setScene(new QGraphicsScene);
+}
+
+MainWindow::~MainWindow() {
+  delete ui.viewAP->scene();
+  delete ui.viewLAT->scene();
 }
 
 void MainWindow::New() {
   View* v[2] = {ui.viewAP, ui.viewLAT};
-  QGraphicsScene* s[2] = {new QGraphicsScene(), new QGraphicsScene()};
+  QGraphicsScene* s[2] = {ui.viewAP->scene(), ui.viewLAT->scene()};
 
   for (int i = 0; i < 2; ++i) {
     QString dir = QFileDialog::getOpenFileName(
@@ -21,6 +29,8 @@ void MainWindow::New() {
     if (dir == nullptr) return;
     QImage* img = new QImage(dir);
 
+    delete s[i];
+    s[i] = new QGraphicsScene;
     v[i]->setScene(s[i]);
     s[i]->addPixmap(QPixmap::fromImage(*img));
     v[i]->fitInView(
