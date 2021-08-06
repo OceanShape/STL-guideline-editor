8134dadbf289261a09ba664ef48736bafbcabe35
OS <oceanshape700@gmail.com>
2021-08-06 18:07:32 +0900

    feat: Add constant width/radius variable

--- view.cpp
+++ view.cpp
@@ -1,17 +1,23 @@
 #include "view.h"
 
 std::vector<QGraphicsEllipseItem*> point;
+const int clickCorrectionWidth = 20;
+const int clickRangeRadius = 25;
+const int pointRadius = 50;
 
 View::View(QWidget* parent) : QGraphicsView(parent) {}
 
 void View::mousePressEvent(QMouseEvent* event) {
+  static int top = -1;
   QPointF pos = mapToScene(event->pos());
+  pos.setX(pos.x() - clickCorrectionWidth);
+  pos.setY(pos.y() - clickCorrectionWidth);
   QPen* pen = new QPen(Qt::green);
   QBrush* brush = new QBrush(Qt::white);
   pen->setWidth(10);
   if (event->button() == Qt::LeftButton) {
-    point.push_back(
-        scene()->addEllipse(pos.x() - 10, pos.y() - 10, 50, 50, *pen, *brush));
+    point.push_back(scene()->addEllipse(pos.x(), pos.y(), pointRadius,
+                                        pointRadius, *pen, *brush));
   } else {
     if (!point.empty()) {
       scene()->removeItem((QGraphicsItem*)point.back());
