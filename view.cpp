11eaabf125cce8b20c39583632ab0e49153eca47
OS <oceanshape700@gmail.com>
2021-08-08 16:13:17 +0900

    feat: Add drawing line logic to drawBaseLine()

--- view.cpp
+++ view.cpp
@@ -7,67 +7,81 @@ qreal pointRadius = 40;
 std::vector<QGraphicsLineItem*> lines;
 
 View::View(QWidget* parent) : QGraphicsView(parent) {
-    lines.reserve(4 * 7);
+  lines.reserve(4 * 7);
 
-    pen = new QPen(Qt::green);
-    pen->setWidth(10);
-    brush = new QBrush(Qt::white);
+  pen = new QPen(Qt::green);
+  pen->setWidth(10);
+  brush = new QBrush(Qt::white);
+
+  isBaseLineDrawn = false;
 }
 
 View::~View() {
-    delete pen;
-    delete brush;
+  delete pen;
+  delete brush;
 }
 
 void View::drawBaseLine(const QPointF& pos, const Qt::MouseButton& btn) {
-    if (btn == Qt::LeftButton) {
-
-    }
+  if (btn == Qt::LeftButton) {
+    scene()->addLine(pos.x(), 0 + 10, pos.x(), static_cast<qreal>(4480 - 10),
+                     *pen);
+    pen->setColor(Qt::red);
+    pen->setStyle(Qt::DotLine);
+    scene()->addLine(0 + 10, pos.y(), static_cast<qreal>(3600 - 10), pos.y(),
+                     *pen);
+    pen->setColor(Qt::green);
+    pen->setStyle(Qt::SolidLine);
+    isBaseLineDrawn = true;
+  }
 }
 
-void View::drawPointAndLine(const QPointF& pos, const Qt::MouseButton& btn) {
-    if (btn == Qt::LeftButton) {
-        if (clickRangedEllipseItemOrNull(pos) == false) {
-            points.push_back({ scene()->addEllipse(pos.x(), pos.y(), pointRadius,
-                                                  pointRadius, *pen, *brush),
-                              pos });
-        }
-        if (points.size() % 4 == 0) {
-            pen->setWidth(7);
-            int idx = points.size() - 4;
-            for (int i = 0; i < 4; ++i) {
-                lines.push_back(scene()->addLine(
-                    points[idx + i].position.x() + clickCorrectionWidth,
-                    points[idx + i].position.y() + clickCorrectionWidth,
-                    points[idx + (i + 1) % 4].position.x() + clickCorrectionWidth,
-                    points[idx + (i + 1) % 4].position.y() + clickCorrectionWidth,
-                    *pen));
-            }
-        }
+void View::drawPointAndLine(QPointF pos, const Qt::MouseButton& btn) {
+  pos.setX(pos.x() - clickCorrectionWidth);
+  pos.setY(pos.y() - clickCorrectionWidth);
+  if (btn == Qt::LeftButton) {
+    if (clickRangedEllipseItemOrNull(pos) == false) {
+      pen->setWidth(10);
+      points.push_back({scene()->addEllipse(pos.x(), pos.y(), pointRadius,
+                                            pointRadius, *pen, *brush),
+                        pos});
     }
-    else {
-        if (points.empty()) return;
-        scene()->removeItem((QGraphicsItem*)points.back().item);
-        points.pop_back();
+    if (points.size() % 4 == 0) {
+      pen->setWidth(7);
+      size_t idx = points.size() - 4;
+      for (size_t i = 0; i < 4; ++i) {
+        lines.push_back(scene()->addLine(
+            points[idx + i].position.x() + clickCorrectionWidth,
+            points[idx + i].position.y() + clickCorrectionWidth,
+            points[idx + (i + 1) % 4].position.x() + clickCorrectionWidth,
+            points[idx + (i + 1) % 4].position.y() + clickCorrectionWidth,
+            *pen));
+      }
     }
+  } else {
+    if (points.empty()) return;
+    scene()->removeItem((QGraphicsItem*)points.back().item);
+    points.pop_back();
+  }
 }
 
 void View::mousePressEvent(QMouseEvent* event) {
-    QPointF pos = mapToScene(event->pos());
-    pos.setX(pos.x() - clickCorrectionWidth);
-    pos.setY(pos.y() - clickCorrectionWidth);
+  QPointF pos = mapToScene(event->pos());
+  Qt::MouseButton btn = event->button();
 
-    drawPointAndLine(pos, event->button());
+  if (isBaseLineDrawn == false)
+    drawBaseLine(pos, btn);
+  else
+    drawPointAndLine(pos, btn);
 }
 
 bool View::clickRangedEllipseItemOrNull(const QPointF& pos) {
-    for (const auto& p : points) {
-        qreal x = p.position.x();
-        qreal y = p.position.y();
-        if ((x - clickRangeWidth <= pos.x() && pos.x() <= x + clickRangeWidth) &&
-            (y - clickRangeWidth <= pos.y() && pos.y() <= y + clickRangeWidth)) {
-            return true;
-        }
+  for (const auto& p : points) {
+    qreal x = p.position.x();
+    qreal y = p.position.y();
+    if ((x - clickRangeWidth <= pos.x() && pos.x() <= x + clickRangeWidth) &&
+        (y - clickRangeWidth <= pos.y() && pos.y() <= y + clickRangeWidth)) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
--- view.h
+++ view.h
@@ -7,28 +7,29 @@
 #include <QPoint>
 
 typedef struct {
-	QGraphicsEllipseItem* item;
-	QPointF position;
+  QGraphicsEllipseItem* item;
+  QPointF position;
 } point;
 
 class View : public QGraphicsView {
-	Q_OBJECT
-public:
-	View::~View();
-	explicit View(QWidget* parent = nullptr);
-	bool clickRangedEllipseItemOrNull(const QPointF& pos);
-	void drawBaseLine(const QPointF& pos, const Qt::MouseButton& btn);
-	void drawPointAndLine(const QPointF& pos, const Qt::MouseButton& btn);
+  Q_OBJECT
+ public:
+  View::~View();
+  explicit View(QWidget* parent = nullptr);
+  bool clickRangedEllipseItemOrNull(const QPointF& pos);
+  void drawBaseLine(const QPointF& pos, const Qt::MouseButton& btn);
+  void drawPointAndLine(QPointF pos, const Qt::MouseButton& btn);
 
-signals:
+ signals:
 
-public slots:
+ public slots:
 
-private:
-	std::vector<point> points;
-	QPen* pen;
-	QBrush* brush;
-	void mousePressEvent(QMouseEvent* event) override;
+ private:
+  std::vector<point> points;
+  QPen* pen;
+  QBrush* brush;
+  bool isBaseLineDrawn;
+  void mousePressEvent(QMouseEvent* event) override;
 };
 
 #endif  // VIEW_H
