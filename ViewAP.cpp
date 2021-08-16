5bf279165309ff423c9b325eb5ef788d4d206f9b
OS <oceanshape700@gmail.com>
2021-08-16 18:04:22 +0900

    feat: Add drawing logic using currentMode to ViewAP class

--- ViewAP.cpp
+++ ViewAP.cpp
@@ -4,15 +4,16 @@ void ViewAP::mousePressEvent(QMouseEvent* event) {
   QPointF pos = mapToScene(event->pos());
   Qt::MouseButton btn = event->button();
 
-  if (isBaseLineDrawn == false)
-    drawBaseLine(pos, btn);
-  else
+  if (currentMode == Mode::BASE_LINE) {
+    if (isBaseLineDrawn == false) drawBaseLine(pos, btn);
+  } else if (currentMode == Mode::SPINE) {
     drawPointAndLine(pos, btn);
+  }
 }
 
 void ViewAP::keyPressEvent(QKeyEvent* event) {
-    if (event->key() == Qt::Key_1)
-        currentMode = Mode::BASE_LINE;
-    if (event->key() == Qt::Key_2)
-        currentMode = Mode::SPINE;
+  if (event->key() == Qt::Key_1)
+    currentMode = Mode::BASE_LINE;
+  else if (event->key() == Qt::Key_2)
+    currentMode = Mode::SPINE;
 }
