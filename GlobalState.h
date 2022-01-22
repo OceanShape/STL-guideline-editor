277273efcf239d94a9288377d4d633fbe51210e2
OceanShape <oceanshape700@gmail.com>
2022-01-22 23:54:20 +0900

    feat: Write GlobalState(singleton)

--- GlobalState.h
+++ GlobalState.h
@@ -1,1 +1,10 @@
-#pragma once
+class GlobalState {
+ public:
+  static GlobalState& instance() {
+    static GlobalState* instance = new GlobalState();
+    return *instance;
+  }
+
+ private:
+  GlobalState() {}
+};
